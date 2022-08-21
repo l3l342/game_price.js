e76ede1b85ae36ae396cf0da559fb398e9d3e17e
Ben <75950537+l3l342@users.noreply.github.com>
2022-08-22 01:05:06 +0200

    Update game_price.js

--- game_price.js
+++ game_price.js
@@ -1,33 +1,58 @@
-let detailFontSize = 20;
-let statusFontSize = 10;
-let detailColor = Color.white();
+const Url = "https://store.steampowered.com/api/appdetails?appids=252490&cc=de"; 
 
 let widget = await buildWidget();
-
 Script.setWidget(widget);
 widget.presentSmall()
-Script.complete();
 
 async function buildWidget() {
     let widget = new ListWidget();
-    widget.backgroundColor = Color.black();
     
-    const price = await getPrice();
-    let wprice = await widget.addText(price);
-  
-    wprice.font = Font.mediumRoundedSystemFont(detailFontSize);
-    wprice.textcolor = detailColor;
+    widget.backgroundImage = await loadImage(await imageUrl(Url));
+    
+    
+    const wheader = widget.addText("Rust Preis");
+    
+    wheader.font = Font.mediumRoundedSystemFont(20); 
+    wheader.centerAlignText();
+    wheader.textColor = Color.white();
+    
+    const price = await getPrice(Url);
+    const wprice = await widget.addText(price);
+    
+    wprice.centerAlignText();
+    wprice.font = Font.mediumRoundedSystemFont(30);
+    wprice.textColor = Color.white();
 
     return widget;
 }
 
 
-async function getPrice() {
-    const url = "https://store.steampowered.com/api/appdetails?appids=252490";
-    const response = new Request(url);
+async function getPrice(Url) {
+    const response = new Request(Url);
     const data = await response.loadJSON()
 
     const price = data["252490"].data.price_overview.final_formatted;
 
     return price;
 }
+
+
+async function imageUrl(Url) {
+    const response = new Request(Url);
+    const data = await response.loadJSON()
+
+    const link = data["252490"].data.screenshots[0].path_thumbnail;
+    var linkarray = link.split("?");
+    
+    const imageurl = linkarray[0];
+    
+    return imageurl;
+}
+
+async function loadImage(imgurl) {
+  const req = new Request(imgurl);
+  return await req.loadImage()
+}
+
+
+Script.complete();
